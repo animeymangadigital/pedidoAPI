c201700988eb99fe184a3d1e04e6817ca92dcf9e
xavier sarmiento <xesp123@gmail.com>
2016-11-10 14:53:09 -0500

    add new property

--- models/productos.js
+++ models/productos.js
@@ -5,7 +5,7 @@ exports = module.exports = function(app, mongoose) {
 		de7a12 :	{ type: Number },
 		de13a17: 	{ type: Number },
 		de18a49: 	{ type: Number },
-		unidad:	{ type: Number },
+		unidad:	{ type: String },
 		type:	{ type: String },
 		minutaId:	{ type: String},
 		updated_at: { type: Date }
