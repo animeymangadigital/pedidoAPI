bf5df2cd05e38937ce828ed9904762f381da3a90
xesp1234 <xesp123@gmail.com>
2017-08-21 20:05:52 -0500

    changes

--- controllers/pedidos.js
+++ controllers/pedidos.js
@@ -2,25 +2,41 @@
 var mongoose = require('mongoose');
 var Productos = mongoose.model('Producto');
 var Minutas = mongoose.model('Minuta');
+const async = require('asyncawait/async');
+const awaits = require('asyncawait/await');
 
-exports.pedidos = function(req, res) {
-    var de7a12  = req.body.de7a12;
-    var de13a17 = req.body.de13a17;
-    var de18a49 = req.body.de18a49;
-    var productosArray = [];
-    var array = [];
-    Minutas.find({
-        cicloId: req.body.cicloId
-    }).exec(function(err, minutas) {
-        minutas.forEach(function(minuta) {
-            minuta.productos.forEach(function(producto) {
+exports.pedidos = function (req, res) {
+
+    let send = async((body) => {
+        var de7a12  = body.de7a12;
+        var de13a17 = body.de13a17;
+        var de18a49 = body.de18a49;
+        var productosArray = [];
+        var array = [];
+
+        let minutas = awaits(Minutas.find({
+            cicloId: body.cicloId
+        }));
+
+        minutas.forEach(function (minuta) {
+            let productos = awaits(Productos.find({
+                minutaId: minuta._id
+            }));
+            productoss.forEach(function (producto) {
                 producto.de7a12 = producto.de7a12 * de7a12;
                 producto.de13a17 = producto.de13a17 * de13a17;
                 producto.de18a49 = producto.de18a49 * de18a49;
                 array.push(producto);
             });
         });
-        array.forEach(function(item) {
+
+        let template = awaits(db.emailTemplates.find({
+            where: {
+                type: data.templateType
+            }
+        }));
+
+        array.forEach(function (item) {
             var d7a12 = 0;
             var d13a17 = 0;
             var d18a49 = 0;
@@ -40,18 +56,18 @@ exports.pedidos = function(req, res) {
                     }
                 }
 
-                if(!exists){
-                   var prod = {
-                       title: item.title.trim(),
-                       unidad:item.unidad,
-                       type: item.type,
-                       de7a12:d7a12,
-                       de13a17:d13a17,
-                       de18a49:d18a49,
-                       remain:0,
-                       total:0
-                   }
-                   productosArray.push(prod);
+                if (!exists) {
+                    var prod = {
+                        title: item.title.trim(),
+                        unidad: item.unidad,
+                        type: item.type,
+                        de7a12: d7a12,
+                        de13a17: d13a17,
+                        de18a49: d18a49,
+                        remain: 0,
+                        total: 0
+                    }
+                    productosArray.push(prod);
                 }
             } else {
                 if (item.unidad === 'huevo') {
@@ -69,18 +85,18 @@ exports.pedidos = function(req, res) {
                         }
                     }
 
-                    if(!exists){
-                       var prod = {
-                           title: item.title.trim(),
-                           unidad:item.unidad,
-                           type: item.type,
-                           de7a12:d7a12,
-                           de13a17:d13a17,
-                           de18a49:d18a49,
-                           remain:0,
-                           total:0
-                       }
-                       productosArray.push(prod);
+                    if (!exists) {
+                        var prod = {
+                            title: item.title.trim(),
+                            unidad: item.unidad,
+                            type: item.type,
+                            de7a12: d7a12,
+                            de13a17: d13a17,
+                            de18a49: d18a49,
+                            remain: 0,
+                            total: 0
+                        }
+                        productosArray.push(prod);
                     }
 
                 } else {
@@ -94,22 +110,30 @@ exports.pedidos = function(req, res) {
                         }
                     }
 
-                    if(!exists){
-                       var prod = {
-                           title: item.title.trim(),
-                           unidad:item.unidad,
-                           type: item.type,
-                           de7a12:item.de7a12,
-                           de13a17:item.de13a17,
-                           de18a49:item.de18a49,
-                           remain:0,
-                           total:0
-                       }
-                       productosArray.push(prod);
+                    if (!exists) {
+                        var prod = {
+                            title: item.title.trim(),
+                            unidad: item.unidad,
+                            type: item.type,
+                            de7a12: item.de7a12,
+                            de13a17: item.de13a17,
+                            de18a49: item.de18a49,
+                            remain: 0,
+                            total: 0
+                        }
+                        productosArray.push(prod);
                     }
                 }
             }
         });
-        res.status(200).jsonp(productosArray);
+
+        return productosArray;
     });
+
+    send({
+            body: req.body
+        })
+        .then(() => {
+            res.status(200).jsonp(productosArray);
+        });
 };
